905ca7c67f09d4c33aec485f28a61db55f58c2d5
Narsilien <rigaux.jonathan@gmail.com>
2026-09-05 07:32:52 +0200

    Fix dialog

--- resources/views/components/ui/alert-dialog/alert-dialog-footer.blade.php
+++ resources/views/components/ui/alert-dialog/alert-dialog-footer.blade.php
@@ -1,5 +1,5 @@
 <div
-	{{ $attributes->twMerge('-mx-4 -mb-4 flex flex-col-reverse gap-2 rounded-b-xl border-t bg-muted/50 p-4 sm:flex-row-reverse sm:justify-between') }}
+	{{ $attributes->twMerge('-mx-4 -mb-4 flex flex-row justify-between gap-2 rounded-b-xl border-t bg-muted/50 p-4') }}
 	data-slot="alert-dialog-footer"
 >
 	{{ $slot }}
--- resources/views/components/ui/dropdown-menu/dropdown-menu-root.blade.php
+++ resources/views/components/ui/dropdown-menu/dropdown-menu-root.blade.php
@@ -1,7 +1,7 @@
 <div
 	{{ $attributes->merge(['data-slot' => 'dropdown-menu-root']) }}
 	x-data="{ open: false }"
-	x-on:dropdown-menu-close="open = false"
+	x-on:dropdown-menu-close.window="open = false"
 	x-on:dropdown-menu-open="open = true"
 >
 	{{ $slot }}
